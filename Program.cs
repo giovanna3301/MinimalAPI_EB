f932e7b2e78e64f198eacacab2bc908a8f21d32f
ErikBohutski <erikbogucki@gmail.com>
2025-10-13 11:21:25 +0200

    Initial commit - Minimal API with health, hello, and user endpoints

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+﻿using MinimalApiDemo.Models;  // <- to jest ważne!
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Endpoint /
 app.MapGet("/", () => "Hello World!");
 
 // Endpoint /health
@@ -8,5 +11,12 @@ app.MapGet("/health", () => new { status = "Healthy" });
 
 // Endpoint /hello/{name}
 app.MapGet("/hello/{name}", (string name) => new { message = $"Hello, {name}!" });
+
+// Endpoint /user
+app.MapGet("/user", () =>
+{
+    var user = new User { Id = 1, Username = "Eryk", Email = "eryk@example.com" };
+    return user;
+});
 
 app.Run();
